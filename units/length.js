4a692f2d8b1e6f6dc69956b9d8a271efc3a9eaa9
ozgrozer <ozgr@live.com>
2017-10-23 20:34:54 +0300

    From meter to the other units.

--- units/length.js
+++ units/length.js
@@ -17,7 +17,19 @@ const values = {
     }
   },
   'meter': {
-    variants: ['meters', 'meter', 'metres', 'metre', 'm']
+    variants: ['meters', 'meter', 'metres', 'metre', 'm'],
+    to: {
+      kilometer: 0.001,
+      meter: 1,
+      centimeter: 100,
+      milimeter: 1000,
+      micrometer: 1000000,
+      nanometer: 1000000000,
+      mile: 0.000621371,
+      yard: 1.09361,
+      foot: 3.28084,
+      inch: 39.3701
+    }
   },
   'centimeter': {
     variants: ['centimeters', 'centimeter', 'centimetres', 'centimetre', 'cm']
