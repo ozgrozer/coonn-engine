b91f69c4da478c81b2020dd2edfba2019853e72b
ozgrozer <ozgr@live.com>
2017-10-24 20:37:03 +0300

    Empty input error was fixed.

--- engine/trevor-engine.js
+++ engine/trevor-engine.js
@@ -2,11 +2,13 @@ const fn = require('./functions')
 const units = require('./units')
 
 module.exports = input => {
-  const [inputValue, inputUnit, to, outputUnit] = input.split(' ')
-  let result
-  if (to === 'to') {
-    result = units.length(inputValue, inputUnit, outputUnit)
-    result = fn.prettyResult(result, outputUnit)
+  let result = ''
+  if (input) {
+    const [inputValue, inputUnit, to, outputUnit] = input.split(' ')
+    if (to === 'to') {
+      result = units.length(inputValue, inputUnit, outputUnit)
+      result = fn.prettyResult(result, outputUnit)
+    }
   }
   return result
 }
